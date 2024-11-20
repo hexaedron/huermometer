2fef89e548165e3010b45026784d735ea1ef51fa
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2024-11-21 01:01:21 +0300

    Temp filter

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <ch32v003fun.h>
+#include <cstring>
 #include "funny_defs.h"
 #include "funny_time.h"
 #include "simpleTimer.h"
@@ -6,20 +7,39 @@
 #include "therm.h"
 
 #define WS2812BSIMPLE_IMPLEMENTATION
-#define LED_BRIGHTNESS 8
+#define LED_BRIGHTNESS 16
 #define TEMP_HIGH 300
 #define TEMP_LOW 170
+#define NUM_LEDS 8
 
 uint8_t map8(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max)
 {
 	return  (uint8_t)((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
 
+uint16_t tempLog[8] = {0};
+void push_temp(int16_t newtemp)
+{
+	static uint8_t tempPointer = 0;
+	tempLog[tempPointer] = newtemp;
+
+	tempPointer++;
+	if(tempPointer >= 8) tempPointer = 0;
+}
+
+int16_t filterTemp(void)
+{
+	int32_t sum = 0;
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		sum += tempLog[i];
+	}
+
+	return (int16_t)(sum >> 3);
+}
+
 #include "ws2812b_simple.h"
-
 #include "color_utilities.h"
-
-#define NUM_LEDS 8
 
 int main()
  {
@@ -36,19 +56,27 @@ int main()
 
 	uint8_t hue = 0;
 	uint32_t rgb;
-	bool start = true;
-	simpleTimer32 tmr(1000UL);
+	uint8_t start = 0;
+	simpleTimer32 tmr(125UL);
 	int16_t temp = myTherm.getTemp();
+	
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		push_temp(temp);
+	}
+	
+
 	while(1)
 	{
 		if(tmr.ready())
 		{
-			temp = myTherm.getTemp();
+			push_temp(myTherm.getTemp());
 		}
 
+		memset(leds, 0, sizeof(leds));
 		for (uint8_t i = (uint8_t)start; i < NUM_LEDS; i+=2)
 		{
-			hue =  map8(constrain(temp, TEMP_LOW, TEMP_HIGH), TEMP_LOW, TEMP_HIGH, 255, 0);
+			hue =  map8(constrain(filterTemp(), TEMP_LOW, TEMP_HIGH), TEMP_LOW, TEMP_HIGH, 255, 0);
 			rgb = EHSVtoHEX(hue, 255, LED_BRIGHTNESS);
 			uint8_t* tmp = (uint8_t*)(&rgb);
 			leds[i*3] = tmp[0];
@@ -56,7 +84,8 @@ int main()
 			leds[i*3+2] = tmp[2];
 		}
 		WS2812BSimpleSend(GPIOC, 6, (uint8_t*)leds, sizeof(leds));
-		//Delay_Ms(50);
-		start = !start;
+		//Delay_Ms(10);
+		start++; 
+		if(start > 1 ) start = 0;
 	}	
-}
+ }
